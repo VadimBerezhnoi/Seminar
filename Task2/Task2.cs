392b2321e65d65f6e4a268f45e45e110c1014c2a
VadimBerezhnoi <tsigma66@gmail.com>
2023-01-30 21:07:02 +0300

    Исправлено решение и добавлена формула

--- Task2/Task2.cs
+++ Task2/Task2.cs
@@ -7,11 +7,9 @@ Console.WriteLine(num);
 int a1 = num / 100;
 int a2 = num % 10;
 
-int max = a1;
 
-if (a2 > max)
-{
-    max = a2;
-}
 
-//Console.Write($"{a1}{a2}");
+
+
+Console.Write($"{a1}{a2}");
+
