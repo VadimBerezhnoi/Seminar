006c7635fc7f5170410a8e5d0b33de201eecffb7
VadimBerezhnoi <tsigma66@gmail.com>
2023-01-30 21:32:05 +0300

    Написали программу проверки кратности числа числам 7 и 23

---
+++ Task4/Task4.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.Write("Введите число a ");
+int a = int.Parse(Console.ReadLine()!);
+
+int b = 7;
+int c = 23;
+
+int result1 = a % b;
+int result2 = a % c;
+
+if (result1 == 0 && result2 == 0)
+{
+Console.Write($"Число {a} кратно {b} и {c}");
+} 
+
+else 
+{
+Console.Write("Некратно");
+}
