f0657f81340eb9bdd70e8cfd2ab14ed4b6fdfc14
VadimBerezhnoi <tsigma66@gmail.com>
2023-01-30 21:19:27 +0300

    Написали программу, определяющую кратность чисел

---
+++ Task3/Task3.cs
@@ -0,0 +1,20 @@
+﻿
+Console.Clear();
+
+Console.Write("Введите число a ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число b ");
+int b = int.Parse(Console.ReadLine()!);
+
+int c = a % b;
+
+if (c == 0)
+{
+    Console.Write("Кратно");
+}
+
+else 
+{
+   Console.Write($"Не кратно, остаток {c}"); 
+}
