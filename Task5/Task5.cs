26c9104e092743b74c37d135cf3473c4d18886fb
VadimBerezhnoi <tsigma66@gmail.com>
2023-01-30 21:41:47 +0300

    Написали программу определяющую, является ли первое число квадратом второго и наоборт

---
+++ Task5/Task5.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Введите число a ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число b ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (b * b == a || a * a == b)
+{
+Console.Write("Да");
+}
+
+else
+{
+Console.Write("Нет");
+}
