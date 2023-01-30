cae5ca5844abe99d89e28370f444715331469f0f
VadimBerezhnoi <tsigma66@gmail.com>
2023-01-30 21:04:36 +0300

    Изначальный вариант

--- Task2/Task2.cs
+++ Task2/Task2.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Console.Clear();
+
+int num = new Random().Next(100, 1000);  // 100 - 999
+
+Console.WriteLine(num);
+
+int a1 = num / 100;
+int a2 = num % 10;
+
+int max = a1;
+
+if (a2 > max)
+{
+    max = a2;
+}
+
+//Console.Write($"{a1}{a2}");
